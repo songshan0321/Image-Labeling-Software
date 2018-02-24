a777cb19e988cf6b29f1ab0d623f8230c890e638
SongShanYou <songshan_you@mymail.sutd.edu.sg>
2018-02-25 01:31:23 +0800

    Update Backend
    Update new labels and implement a new database

--- backend01.py
+++ backend01.py
@@ -1,77 +1,75 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 10 14:37:30 2018
+Created on Sat Feb 24 23:15:28 2018
 
-@author: Songshan
+@author: Windows 10
 """
-
-# BACKEND TRIAL
-
 from Tkinter import *
 import sqlite3 #Import the SQLite3 module
 
 
 def update():
     name = pic_field.get()
+    
     try:
-        print "Go into try"
-        cur.execute("INSERT INTO main(pic_name) VALUES (?)", (name,))
+        print "Go into 'try'"
+        cur.execute("INSERT INTO result(image) VALUES (?)", (name,))
         db.commit()
     except:
-        cur.execute("UPDATE main SET walk=0,train=0,rest=0 WHERE pic_name = (?)", (name,))
+        cur.execute("UPDATE result SET date = NULL, person = 0, not_useful = 0, house = 0, park = 0, market = 0, food_center = 0, hospital = 0, playground = 0, religous = 0, road = 0, bus_stop = 0, mrt = 0, walk = 0, cycle = 0, sit = 0, chat = 0, eat = 0, shopping = 0, rest = 0, other_activity = 0 WHERE image = (?)", (name,))
         db.commit()
 
-    # Take checkboxes input and inser into table one by one
-    for act, num in btn_dic.iteritems(): # {'walk': num}
+            
+    # Take checkboxes input and insert into table one by one
+    for lbl in btn_ls:
+        label = lbl[0]
+        num = lbl[1]
         if num.get() == 1:
-            cur.execute("UPDATE main SET " + act + " = (?) WHERE pic_name = (?)", (1,name))
+            cur.execute("UPDATE result SET " + label + " = (?) WHERE image = (?)", (1,name))
             db.commit()
             
     
     print 'Inserted ' + name
     
-    
-db = sqlite3.connect('activity.db')
-
+db = sqlite3.connect('Labelling.db')
 cur = db.cursor()
 
 master = Tk()
 master.title("My APP")
-master.geometry('400x200')
-
-
-# Load data from activity table and return a dict
-cur.execute("SELECT * FROM activity")
+master.geometry('600x600')
+    
+# Load data from activity table and return a list of label
+cur.execute("SELECT * FROM labels")
 rows = cur.fetchall()
 
-act_dic = {}
+lbl_ls = []
 for row in rows: 
-    act = row[1]
-    num = "var" + str(row[0])
-    act_dic[act] = num
-#print "act_dic =",
-#print act_dic
-
+    lbl = row[1]
+    lbl_ls.append(lbl)
+#print "lbl_ls =",
+print lbl_ls
 
 # GUI Part
 
-pic_field = Entry(master, text="Pic Name")
+pic_field = Entry(master, text="Image Name")
 pic_field.pack()
 
-btn_dic={} # {'walk': num}
-# Generate multiple buttons from act_dic
-for act, num in act_dic.iteritems():
+btn_ls=[] # [('walk': num)]
+# Generate multiple buttons from lbl_ls
+for label in lbl_ls:
     num = IntVar()
-    c= Checkbutton(master, text=act, variable=num)
-    btn_dic[act] = num
+    c= Checkbutton(master, text=label, variable=num)
+    btn_ls.append((label,num))
     c.pack()
+    
+#print "btn_ls =",
+print btn_ls
 
 b = Button(master, text="Save", command=update)
 b.pack()
 
 
 mainloop()
-
 
 
 
